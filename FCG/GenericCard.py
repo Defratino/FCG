a02187c97836ebebd8fb3b287174e824b9b8a797
DaniDude <35263467+Defratino@users.noreply.github.com>
2018-04-21 17:42:22 +0300

    Consept Pre-Alpha 1.1

---
+++ FCG/GenericCard.py
@@ -0,0 +1,10 @@
+class GenericCard:
+
+    def __int__(self, card):
+        self.power = card[0]
+        self.symbol = card[1]
+
+    # adds a unit to the next game depends on it stats
+    def create_unit(self):
+        # names will be changed
+        level.units.add(Unit(self))
