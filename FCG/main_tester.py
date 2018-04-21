d2151029c4f48180c5ca28f761786533aa999ea1
DaniDude <35263467+Defratino@users.noreply.github.com>
2018-04-21 18:45:46 +0300

    Pre-Alpha 1.0

--- FCG/main_tester.py
+++ FCG/main_tester.py
@@ -1,3 +1,37 @@
-import CardUtil
+import pygame
+import Player
 
-print CardUtil.get_random_card()
+pygame.init()
+
+DISPLAY_WIDTH = 800
+DISPLAY_HEIGHT = 600
+
+game_display = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
+clock = pygame.time.Clock()
+FPS = 60
+
+p1 = Player.Player(game_display, 3, 10, (23, 102, 67))
+
+while True:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+
+    # start event
+
+    p1.move()
+
+    # end event
+
+    game_display.fill((10, 240, 220))
+
+    # start draw
+
+    p1.draw()
+
+    # end draw
+
+    dtime = clock.tick(FPS)
+    pygame.display.update()
