3cea86a9a0f906ef39ac239fdf1f1219957ef24f
DaniDude <35263467+Defratino@users.noreply.github.com>
2018-04-22 21:34:49 +0300

    Pre-Alpha 1.6
    Player movement mainly finished.

--- FCG/Player.py
+++ FCG/Player.py
@@ -19,28 +19,42 @@ class Player:
         self.level = level
         self.x_speed = 0
         self.y_speed = 0
+        self.last_keys = {"UP":False, "DOWN":False}
+        self.frame_keys = {"UP":True, "DOWN":True}
 
     def move(self):
+        # key managment
         k = pygame.key.get_pressed()
+        # keys which are pressed only at this frame
+        self.frame_keys["UP"] = (k[pygame.K_UP] or k[pygame.K_w]) and not self.last_keys["UP"]
+        self.frame_keys["DOWN"] = (k[pygame.K_DOWN] or k[pygame.K_s]) and not self.last_keys["DOWN"]
+
+        # the keys which are pressed last frame
+        self.last_keys["UP"] = k[pygame.K_UP] or k[pygame.K_w]
+        self.last_keys["DOWN"] = k[pygame.K_DOWN] or k[pygame.K_s]
+
+        # horizontal speed & movement
         self.x_speed = 0
         if k[pygame.K_RIGHT] or k[pygame.K_d]:
             self.x_speed += self.speed
         if k[pygame.K_LEFT] or k[pygame.K_a]:
             self.x_speed -= self.speed
 
-        if pygame.mouse.get_pressed()[0]:
-            self.shoot()
-
+        # vertical speed: jump & fall
         if collision_all(self, self.level.platforms, 0, 1) and not collision_all(self, self.level.platforms):
-            self.y_speed = 0
-            if k[pygame.K_UP] or k[pygame.K_w]:
+            if self.frame_keys["UP"]:
                 self.y_speed = -12
-            if k[pygame.K_DOWN] or k[pygame.K_s]:
+            if self.frame_keys["DOWN"]:
                 self.y_speed = 1
         else:
             self.y_speed += 1
 
+        # shoot bullet
+        if pygame.mouse.get_pressed()[0]:
+            self.shoot()
+
     def collisions(self):
+        # key managment
         k = pygame.key.get_pressed()
 
         # x collision
@@ -53,7 +67,7 @@ class Player:
         self.x += self.x_speed
 
         # y collision
-        if not (k[pygame.K_DOWN] or k[pygame.K_s]) and self.y_speed > 0:
+        if (not self.frame_keys["DOWN"]) and (self.y_speed > 0):
             if collision_all(self, self.level.platforms, 0, self.y_speed) and not collision_all(self,
                                                                                                 self.level.platforms):
                 while not (collision_all(self, self.level.platforms, 0, numpy.sign(self.y_speed))):
